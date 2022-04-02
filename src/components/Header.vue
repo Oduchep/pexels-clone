7f983685df4b3d1f00960f9c68c914c699f30996
Peter Okerulu <oduchep@gmail.com>
2022-04-02 14:34:52 +0100

    Final environment setup; Created the header component; Created the input component;

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <nav class="px-6 py-3 flex items-center justify-between bg-[#232A34]">
+      <router-link class="flex items-center text-white hover:text-gray-200" to="/">
+        <svg class="rounded-lg" xmlns="http://www.w3.org/2000/svg" width="40px" height="40px" viewBox="0 0 32 32">
+          <path d="M2 0h28a2 2 0 0 1 2 2v28a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z" fill="#05A081"></path>
+          <path d="M13 21h3.863v-3.752h1.167a3.124 3.124 0 1 0 0-6.248H13v10zm5.863 2H11V9h7.03a5.124 5.124 0 0 1 .833 10.18V23z" fill="#fff"></path>
+        </svg>
+
+        <p class="hidden lg:block ml-3 text-lg font-bold"> Pexels </p>
+      </router-link>
+
+      <Input placeholder="Search for free photos"/>
+
+      <ul class="hidden lg:flex items-center">
+        <li class="mx-3 text-lg text-white hover:text-gray-200"> Explore </li>
+        <li class="mx-3 text-lg text-white hover:text-gray-200"> Licence </li>
+        <li class="mx-3 text-lg text-white hover:text-gray-200"> Upload </li>
+      </ul>
+      
+      <button class="hidden md:block text-lg text-white">
+        <i class="fa-solid fa-ellipsis"></i>
+      </button>
+
+      <button class="hidden md:block px-8 py-2 rounded font-semibold text-base text-white hover:text-gray-200 bg-btn-green"> Join </button>
+
+      <button class="md:hidden text-lg text-white">
+        <i class="fa-solid fa-bars"></i>
+      </button>      
+    </nav>
+  </header>
+</template>
+
+<script>
+import Input from './Input'
+export default {
+  name: 'Header',
+  components: {
+    Input,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1> I am the home and I have a button </h1>
+    <button class="bg-slate-500">
+      <router-link to="/about">Link to the About Page</router-link>
+    </button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
